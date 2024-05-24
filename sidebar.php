f3ee2f9c1b57f2e665b2ffa1037f81b03691fcd7
Lorenz928 <lorenzbonifacio928@gmail.com>
2024-05-24 17:13:55 +0800

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -11,8 +11,12 @@
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 		<link rel="stylesheet" href="css/style.css">
   </head>
+
+    <style>
+
+    </style>
+
   <body>
-		
 		<div class="wrapper d-flex align-items-stretch">
 			<nav id="sidebar" class="active">
 				<div class="custom-menu">
@@ -45,99 +49,7 @@
 					</ul>
 				</div>
 			</nav>
-
-        <!-- Page Content  -->
-      <div id="content" class="p-4 p-md-5 pt-5">
-		<table class="table">
-			<thead>
-				<tr>
-					<th>User ID</th>
-					<th>Full Name</th>
-					<th>Role</th>
-					<th>Username</th>
-					<th>Password</th>
-					<th>Email</th>
-					<th>Edit</th>
-					<th>Delete</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr>
-					<td>1</td>
-					<td>Lorenz Bonifacio</td>
-					<td>Admin</td>
-					<td>lbonifacio</td>
-					<td>1234</td>
-					<td>lbonifacio@gmail.com</td>
-					<td><button type="button" class="btn btn-success" data-bs-dismiss="modal">Edit</button></td>
-					<td><button type="button" class="btn btn-danger" data-bs-dismiss="modal">Delete</button></td>
-				</tr>
-				<tr>
-					<td>2</td>
-					<td>Laurenz Briones</td>
-					<td>Admin</td>
-					<td>lbriones</td>
-					<td>5678</td>
-					<td>lbriones@gmail.com</td>
-					<td><button type="button" class="btn btn-success" data-bs-dismiss="modal">Edit</button></td>
-					<td><button type="button" class="btn btn-danger" data-bs-dismiss="modal">Delete</button></td>
-				</tr>
-				<tr>
-					<td>3</td>
-					<td>Jason Agilada</td>
-					<td>Customer</td>
-					<td>jasonalamillo123</td>
-					<td>101010</td>
-					<td>jasonalamillo@gmail.com</td>
-					<td><button type="button" class="btn btn-success" data-bs-dismiss="modal">Edit</button></td>
-					<td><button type="button" class="btn btn-danger" data-bs-dismiss="modal">Delete</button></td>
-				</tr>
-				<tr>
-					<td>4</td>
-					<td>Ravin Agon</td>
-					<td>Customer</td>
-					<td>ravinagon123</td>
-					<td>908070</td>
-					<td>ravinagon@gmail.com</td>
-					<td><button type="button" class="btn btn-success" data-bs-dismiss="modal">Edit</button></td>
-					<td><button type="button" class="btn btn-danger" data-bs-dismiss="modal">Delete</button></td>
-				</tr>
-				<tr>
-					<td>5</td>
-					<td>Kirby Pada</td>
-					<td>Customer</td>
-					<td>kirbypada123</td>
-					<td>654321</td>
-					<td>kirbypada@gmail.com</td>
-					<td><button type="button" class="btn btn-success" data-bs-dismiss="modal">Edit</button></td>
-					<td><button type="button" class="btn btn-danger" data-bs-dismiss="modal">Delete</button></td>
-				</tr>
-				<tr>
-					<td>6</td>
-					<td>Cassey Dela Cuz</td>
-					<td>Customer</td>
-					<td>casseydelacuz123</td>
-					<td>246810</td>
-					<td>casseydelacuz@gmail.com</td>
-					<td><button type="button" class="btn btn-success" data-bs-dismiss="modal">Edit</button></td>
-					<td><button type="button" class="btn btn-danger" data-bs-dismiss="modal">Delete</button></td>
-				</tr>
-				<tr>
-					<td>7</td>
-					<td>Marko Alamillo</td>
-					<td>Customer</td>
-					<td>markoalamillo123</td>
-					<td>135790</td>
-					<td>markoalamillo@gmail.com</td>
-					<td><button type="button" class="btn btn-success" data-bs-dismiss="modal">Edit</button></td>
-					<td><button type="button" class="btn btn-danger" data-bs-dismiss="modal">Delete</button></td>
-				</tr>
-			</tbody>
-		</table>
-
-	</div>
-
-</div>
+    </div>
 </div>
 		
 
