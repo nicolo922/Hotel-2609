6b60198fc9666217b44c5a18fccc65472e0eeba1
renzybriones22 <renzybriones@gmail.com>
2024-05-24 17:04:57 +0800

    Update sidebar.php

---
+++ sidebar.php
@@ -0,0 +1,33 @@
+		
+		<div class="wrapper d-flex align-items-stretch">
+			<nav id="sidebar" class="active">
+				<div class="custom-menu">
+					<button type="button" id="sidebarCollapse" class="btn btn-primary">
+						<i class="fa fa-bars"></i>
+						<span class="sr-only">Toggle Menu</span>
+					</button>
+				</div>
+				<div class="p-4">
+					<h1><a href="index.html" class="logo"><img src="images/logohotel.png"></h1> 
+					<ul class="list-unstyled components mb-5">
+						<li>
+							<a href="HotelHome.php"><span class="fa fa-home mr-3"></span> Home</a>
+						</li>
+						<li class="active">
+							<a href="UserTable.php"><span class="fa fa-user mr-3"></span> User Management</a>
+						</li>
+						<li>
+							<a href="RoomTable.php"><span class="fa fa-briefcase mr-3"></span> Rooms Table</a>
+						</li>
+						<li>
+							<a href="ReservationTable.php"><span class="fa fa-sticky-note mr-3"></span> Reservation Table</a>
+						</li>
+						<li>
+							<a href="#"><span class="fa fa-paper-plane mr-3"></span> Amenity Table</a>
+						</li>
+						<li>
+							<a href="#"><span class="fa fa-cog mr-3"></span> Settings</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
