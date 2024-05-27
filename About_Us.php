f575230f47ce2b9dd66eff923261299370f979f3
renzybriones22 <renzybriones@gmail.com>
2024-05-27 21:47:39 +0800

    footer

--- About_Us.php
+++ About_Us.php
@@ -79,7 +79,7 @@ if(isset($_SESSION['username']) && isset($_SESSION['password'])){
             </div>
 
               <div class="owner">
-              <a href="https://www.pornhub.com/" target="_blank">
+              <a href="https://www.facebook.com/renzy.briones.9" target="_blank">
                 <img src="images/renzy.jpg" alt="Owner 2">
                 </a>
                 <h3>Ralph Lorenz M. Bonifacio</h3>
@@ -93,6 +93,38 @@ if(isset($_SESSION['username']) && isset($_SESSION['password'])){
 
       </div>
     </div>
+
+        <!-- FOOTER -->
+<br>
+<br>
+
+  <footer class="footer">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4 text-center">
+        <h3>Contact Us</h3>
+        <p>Email: llhotel.com</p>
+        <p>Phone: +1234567890</p>
+        <p>Address: 123 Main Street, Big Red Sport, Jupiter</p>
+      </div>
+      <div class="col-md-4 text-center">
+        <h3>Follow Us</h3>
+        <ul class="social-links">
+          <li><a href="Contact_Us.php" target="_blank"><i class="fa fa-facebook"></i></a></li>
+          <li><a href="Contact_Us.php" target="_blank"><i class="fa fa-twitter"></i></a></li>
+          <li><a href="Contact_Us.php" target="_blank"><i class="fa fa-instagram"></i></a></li>
+          <li><a href="Contact_Us.php" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+        </ul>
+      </div>
+      <div class="col-md-4 text-center">
+        <h3>Legal</h3>
+        <p>&copy; 2024 LL Hotel. All rights reserved.</p>
+        <p><a href="#">Privacy Policy</a></p>
+        <p><a href="#">Terms of Service</a></p>
+      </div>
+    </div>
+  </div>
+</footer>
 
     <script src="js/jquery.min.js"></script>
     <script src="js/popper.js"></script>
